83c65b6e58d82dae5ce1ebfd4c32dc8e6c467ed3
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2016-12-22 02:29:20 +0100

    Add test outline for client generator

---
+++ test/lye/client_generator_test.exs
@@ -0,0 +1,21 @@
+defmodule Lye.ClientGenerator do
+  use ExUnit.Case, async: true
+
+  import Lye.ClientGenerator
+
+  setup_all do
+    article_wsdl = File.read!(Path.join(@fixture_path, "article_service.xml"))
+    measurement_wsdl = File.read!(Path.join(@fixture_path, "measurement_service.xml"))
+    {:ok, %{article: article_wsdl,
+      blz: blz_wsdl,
+      measurement: measurement_wsdl
+    }}
+  end
+
+  test "generates client for wsdl with one operation", context do
+  end
+
+  test "generates client for wsdl with many operations", context do
+  end
+
+end
