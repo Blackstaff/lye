fb6a56326c214dc817d6be0456da2f45065fd3ef
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2017-01-05 14:52:20 +0100

    Add SweetXml and XMLBuilder to applications

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Lye.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :sweet_xml, :xml_builder],
      mod: {Lye, []}]
   end
 
