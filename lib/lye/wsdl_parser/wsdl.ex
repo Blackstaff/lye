f4e3b4cd97d537ea673049b4c4681a45a4a9f8b4
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2017-01-14 19:11:29 +0100

    Define types of wsdl structures

--- lib/lye/wsdl_parser/wsdl.ex
+++ lib/lye/wsdl_parser/wsdl.ex
@@ -1,23 +1,35 @@
 defmodule Lye.WSDLParser.WSDL do
+  @type t :: %Lye.WSDLParser.WSDL{}
+
   defstruct [port_type: nil, binding: nil, service: nil, tns: nil]
 end
 
 defmodule Lye.WSDLParser.PortType do
+  @type t :: %Lye.WSDLParser.PortType{}
+
   defstruct [name: nil, operations: []]
 end
 
 defmodule Lye.WSDLParser.Binding do
+  @type t :: %Lye.WSDLParser.Binding{}
+
   defstruct [:name, :port_type, :transport, :style]
 end
 
 defmodule Lye.WSDLParser.Service do
+  @type t :: %Lye.WSDLParser.Service{}
+
   defstruct [:name, :port]
 end
 
 defmodule Lye.WSDLParser.Port do
+  @type t :: %Lye.WSDLParser.Port{}
+
   defstruct [:name, :binding, :address]
 end
 
 defmodule Lye.WSDLParser.Operation  do
+  @type t :: %Lye.WSDLParser.Operation{}
+
   defstruct [:name, :input_message, :output_message, :action]
 end
