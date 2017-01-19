51d1413c0130ea5854df9dedf58245111645cb7e
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2017-01-19 01:41:04 +0100

    Move API functionality to main Module
    generate_client and call

--- lib/lye.ex
+++ lib/lye.ex
@@ -1,6 +1,10 @@
 defmodule Lye do
   use Application
 
+  alias Lye.WSDLParser
+  alias Lye.ClientGenerator
+  alias Lye.Client
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -17,4 +21,14 @@ defmodule Lye do
     opts = [strategy: :one_for_one, name: Lye.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  def generate_client(wsdl_url) do
+    {:ok, response} = HTTPoison.get(wsdl_url)
+    {:ok, wsdl} = response.body
+    |> WSDLParser.parse()
+    wsdl
+    |> ClientGenerator.generate()
+  end
+
+  defdelegate call(client, operation_name, parameters), to: Client
 end
